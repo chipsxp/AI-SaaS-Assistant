409f06e35fa3c7f95859b3ab5a896ddb9fc2b7de
chipsxp <chips_xp@yahoo.com>
2024-03-25 00:28:27 -0400

    rid of useless space

--- app/(dashboard)/(routes)/code-generator/constants.ts
+++ app/(dashboard)/(routes)/code-generator/constants.ts
@@ -3,5 +3,5 @@ import * as z from 'zod';
 export const codeGeneratorFormSchema = z.object({
     prompt: z.string().min(1, { 
         message: 'Please describe a code function',
-     }),
+    }),
 })
