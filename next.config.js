b2a465c292ad2d754cccf5e56d85d238e4d08983
Jimmy B. <chips_xp@yahoo.com>
2024-04-12 00:57:16 -0400

    change domains to remotePatterns new logo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["oaidalleapiprodscus.blob.core.windows.net"],
+        remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'oaidalleapiprodscus.blob.core.windows.net',
+        }
+    ],
     }
 }
 
