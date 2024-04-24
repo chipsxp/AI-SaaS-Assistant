2dff6ed2b2bb905187548e47f3440f8f0464da5f
chipsxp <chips_xp@yahoo.com>
2024-04-24 15:03:15 -0400

    NO CH

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,30 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { cn } from '@/lib/utils'
-import { ClerkProvider } from '@clerk/nextjs'
-import { ModalProvider } from '@/components/modal-provider'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/components/modal-provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'AI Saas Controller',
-  description: 'Generated AI Power Productivity App',
-}
+  title: "AI Saas Controller",
+  description: "Generated AI Power Productivity App",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={cn("bg-secondary", inter.className)}>
           <ModalProvider />
-            {children}
-          </body>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
