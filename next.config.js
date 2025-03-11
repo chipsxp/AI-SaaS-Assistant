6b4194f8450cc5e76f2d051f8e4d108bb829a0cb
Jimmy B. <chips_xp@yahoo.com>
2025-03-11 00:21:57 -0400

    one change at next

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "*.replicate.com",
+      },
+      {
+        protocol: "https",
         hostname: "replicate.delivery",
       },
       {
